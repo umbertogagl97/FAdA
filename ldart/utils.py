24e5dc7b95a17937cbe174f7571d695cb21d61c5
umbertogagl97 <u.gagliardini@studenti.unina.it>
2021-11-11 18:27:36 +0100

    Update utils.py

--- ldart/utils.py
+++ ldart/utils.py
@@ -1,4 +1,4 @@
-#library
+#----------------------------------------------------library
 
 import torch
 import torch.nn as nn
@@ -27,7 +27,7 @@ from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import random
 
-#transforms
+#----------------------------------------------------------------transforms
 
 transf_init=transforms.Resize(size=(1000,1000),interpolation=InterpolationMode.NEAREST)
 
@@ -41,7 +41,7 @@ transf_load= transforms.Compose([transforms.ToTensor(),
           ])
 trans_norm=transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
-#functions
+#--------------------------------------------------------------functions
 
 def compute_mask(img):
   #img iniziale [0,1]
@@ -79,6 +79,7 @@ def compute_mask(img):
                   -1, (255, 255),-1)
   #restituisce immagine [0,1]
   return ((image_binary-np.min(image_binary))/(np.max(image_binary)-np.min(image_binary)))
+
 
 def test_average(classifier,input):
   '''
@@ -119,6 +120,7 @@ def test_average(classifier,input):
 
   return predicted,probabilities,values
 
+
 def compute_perturb(x,x_adv):
   '''
     x: img originali
@@ -133,3 +135,103 @@ def compute_perturb(x,x_adv):
     perturb=np.array(transf_init(torch.Tensor(perturb)))
   
   return perturb
+
+
+def print_subplot(perturb,x_test,y_test,preds,x_test_adv,value_preds_adv):
+  '''
+    perturb: perturbazioni
+    x_test: img originali
+    y_test: classi originali
+    preds: classi predette per img originali
+    x_test_adv: img contraddittoria
+    value_preds_adv: probabilità predette img contraddittorie
+    normalize: se True normalizza le immagini
+  '''
+
+  #nel seguente ciclo for si crea un vettore delle classi predette ordinato per probabilità decrescente
+  for i in range(len(x_test)):#(x_test.shape[0]):
+    value=value_preds_adv[i]*100
+    value_sorted=sorted(value,reverse=True)
+    classes=[]
+    for j in range(value.size) :
+      ind=np.where(value==value_sorted[j]) #restituisce l'indice in value del valore uguale a value_sorted[i], quindi è la classe
+      classes.append(classes_name[ind[0][0]]) #classes è il vettore finale
+    
+    value_sorted=[ round(elem, 2) for elem in value_sorted ]
+
+    val_pert=np.mean(np.abs(perturb[i]))
+    pert_min,pert_max=np.min(perturb[i]),np.max(perturb[i])
+    #perturb,val_pert,perturb_norm=compute_perturb(x_test,x_test_adv)
+    perturb[i]=np.clip(perturb[i],0,1) #rimuovo i valori negativi poiché non posso visualizzarli
+    #in seguito per ogni immagine del test set si stampa un subplot
+    fig = plt.figure(figsize=[20,20])
+    #plt.subplots_adjust(wspace=0.9)
+    print('\033[1m'+"IMMAGINE "+'\033[1m',i) #valore END: '\033[0m'
+    #originale
+    ax1 = fig.add_subplot(131) #subplot con 3 righe e due colonne
+    ax1.axis('off')
+    #ax1.imshow(cv2.rotate(x_test[i],cv2.cv2.ROTATE_90_CLOCKWISE))
+    ax1.imshow(x_test[i].transpose(1,2,0))
+    ax1.title.set_text("ORIGINALE\nclasse reale: "+classes_name[np.argmax(y_test[i])]+"\nclasse predetta: "+str(preds[i]))
+    #perturbazione
+    ax2 = fig.add_subplot(132)
+    #ax2.imshow(cv2.rotate(perturb_norm[i],cv2.cv2.ROTATE_90_CLOCKWISE))
+    ax2.imshow(perturb[i].transpose(1,2,0),cmap='gray')
+    #ax2.imshow(perturb[i],cmap='gray')
+    ax2.axis('off')
+    ax2.title.set_text("PERTURBAZIONE\nvalore medio: "+str(round(val_pert,4))+"\nmin: "+str(pert_min)+"\nmax: "+str(pert_max))
+    #perturbata
+    ax3 = fig.add_subplot(133)
+    #ax3.imshow(cv2.rotate(x_test_adv[i],cv2.cv2.ROTATE_90_CLOCKWISE))#,aspect='auto')
+    ax3.imshow(x_test_adv[i].transpose(1,2,0))
+    ax3.axis('off')
+    ax3.title.set_text("PERTURBATA\nclassi predette: "+str(classes)+"\ncon valori: "+str(value_sorted))
+    plt.show()
+          
+
+def save_read(x):
+  print("valori img originale:")
+  print(test_average(classifier,torch.Tensor(x).unsqueeze_(0)))
+  x=x.transpose(1,2,0)*255
+  #plt.imsave('prova.bmp',x)
+  cv2.imwrite('prova.png',x)
+  #plt.imshow(x)
+  #cv2_imshow(x)
+
+  x=cv2.imread('prova.png')
+  x=x/255
+  #x=plt.imread('prova.png')/255
+  #plt.imshow(prova_arr*255)
+  #cv2_imshow(x*255)
+  print("valori dopo salvataggio/lettura:")
+  print(test_average(classifier,torch.Tensor(x.transpose(2,0,1)).unsqueeze_(0)))
+          
+          
+def accuracy_class(class_str,pd_class):
+  '''
+  class_str: string class
+  pd_class: dataframe
+  '''
+  n=np.sum(pd_class['real']==class_str)
+  print("# img"+class_str+": "+str(n))
+
+  p=pd_class.loc[pd_class['real']==class_str]
+  p=p.loc[p['predicted']==p['real']]
+  n_class=p.count(0)[0]
+  print("Numero di predizioni "+class_str+" giuste: "+str(n_class))
+  print("Accuracy : "+str(round(n_class/n*100,2)))
+          
+          
+def print_acc(pd_preds):
+  '''
+  stampa accuracy usando il dataframe con le predizioni
+  pd_preds: dataframe
+  '''
+  true_label = pd_preds.real.values
+  predicted = pd_preds.predicted.values
+  accuracy=round((np.sum((true_label == predicted).astype(int)))/pd_preds.shape[0],4)*100
+  print("\nAccuracy: {0}".format(accuracy))
+  print("Shape dataframe: {0}".format(pd_preds.shape))
+          
+          
+          
