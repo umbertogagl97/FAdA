60d6134846f22d6552efa43d3deda7c679cfe587
umbertogagl97 <u.gagliardini@studenti.unina.it>
2022-01-31 14:50:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-        name="LivDet_ART",
-        version="0.0.1",
-        description="Custom ART for Fingerprint Liveness Detection",
-        packages=["ldart"],
-        package_dir={"ladart":"ldart"},
+        name="FAdA",
+        version="0.1.0",
+        description="Fingerprint ADversarial Attacks",
+        packages=["fada"],
+        package_dir={"fada":"fada"},
         author="Umberto Gagliardini",
-        author_email="u.gagliardini@studenti.unina.it",
-        url="https://github.com/umbertogagl97/LivDet_art/",
+        author_email="umberto.salv@gmail.com",
+        url="https://github.com/umbertogagl97/FAda/",
         install_requires=[  "numpy >= 1.8" ],
         include_package_data=True,
 )
